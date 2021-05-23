11b327fe50d3c217b73b538113627423510f9920
ludi6969 <ludwika.pilchowna@gmail.com>
2021-05-23 12:55:56 +0200

    changes n added

--- common.py
+++ common.py
@@ -5,7 +5,7 @@ def init(db_path):
      if (not utils.is_database_exists(db_path)):
          print('Database was not found. Do you want to create a new one?')
          a = input ('Yes/no: ')
-         if (a == 'no'):
+         if (a == 'no' or a == 'n'):
              sys.exit(1)
          else:
              print('Your database was created succesfuly!')
